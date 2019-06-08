774e5ac55982fd304f30db07588a04cb08dbd835
Blake Erickson <o.blakeerickson@gmail.com>
2019-06-08 09:13:14 -0600

    Added stop server command

--- lib/minecraft_engine_cli.rb
+++ lib/minecraft_engine_cli.rb
@@ -17,7 +17,8 @@ class MinecraftEngineCLI
     @droplet_kit.droplets.all(tag_name: 'minecraft')
   end
 
-  def list_backups
+  def list_backups(name = nil)
+    backups = []
     result = @dropbox_api.list_folder ""
     files = result.entries
     
@@ -26,15 +27,56 @@ class MinecraftEngineCLI
         minecraft = "minecraft"
         if f.name.length > minecraft.length
           if f.name[0..minecraft.length-1] == minecraft
-            puts "#{f.name}"
+            if name != nil
+              if f.name.split('-')[1] == name
+                backups << "#{f.name}"
+              end
+            else
+              backups << "#{f.name}"
+            end
           end
         end
       end
     end
+    backups
+  end
+
+  def count_backups(name = nil)
+    backups = list_backups(name)
+    backups.count
+  end
+
+  def last_backup(name)
+    list_backups(name).last
+  end
+
+  def backup(name, ip)
+    system "ssh -o StrictHostKeyChecking=no root@#{ip} \"ruby /backup.rb #{name}\""
   end
 
   def get_link(filename)
     link = @dropbox_api.get_temporary_link(filename)
+  end
+
+  def stop(name)
+    server = show(name)
+    puts server.name
+    backup_count = new_backup_count = count_backups(name)
+    if server.name.split('-')[1] == name
+      puts backup(name, server.networks.v4[0].ip_address)
+      puts server.id
+      while new_backup_count <= backup_count
+        new_backup_count = count_backups(name)
+        sleep(5)
+      end
+      puts last_backup(name)
+      puts "deleting server #{server.id}"
+      delete(server.id)
+    end
+  end
+
+  def delete(id)
+    @droplet_kit.droplets.delete(id: id)
   end
 
   private
@@ -42,9 +84,8 @@ class MinecraftEngineCLI
   def show(name)
     server = ""
     servers = list_servers
-    list = servers.split("\n")
-    list.each do |s|
-      if s.include?(name)
+    servers.each do |s|
+      if s.name.include?(name)
         server = s
       end
     end
